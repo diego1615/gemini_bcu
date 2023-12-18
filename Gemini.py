48eed8a709e1cd4f2da42852ba9e1bdc1c0759a2
diego1615 <46656062+diego1615@users.noreply.github.com>
2023-12-18 17:41:36 -0300

    Add files via upload

--- Gemini.py
+++ Gemini.py
@@ -1,74 +1,105 @@
-#Author - MrSentinel
-
-import streamlit as st 
-import google.generativeai as genai 
-import google.ai.generativelanguage as glm 
+import streamlit as st
+import google.generativeai as genai
+import google.ai.generativelanguage as glm
 from dotenv import load_dotenv
 from PIL import Image
-import os 
-import io 
+import os
+import io
+#import pandas as pd
+#import base64
+#from pandasai import SmartDatalake
+#from pandasai.responses.streamlit_response import StreamlitResponse
+from pandasai.llm import AzureOpenAI
 
+# Carga variables de entorno
 load_dotenv()
 
+# Conversión de imagen a bytes
 def image_to_byte_array(image: Image) -> bytes:
     imgByteArr = io.BytesIO()
     image.save(imgByteArr, format=image.format)
-    imgByteArr=imgByteArr.getvalue()
+    imgByteArr = imgByteArr.getvalue()
     return imgByteArr
 
+# Configuración de la API de Google
 API_KEY = os.environ.get("GOOGLE_API_KEY")
 genai.configure(api_key=API_KEY)
 
-st.image("./Google-Gemini-AI-Logo.png", width=200)
-st.write("")
+# Configuración de Azure OpenAI
+OPENAI_API_BASE = "https://openaibcu.openai.azure.com/"
+OPENAI_API_KEY = 'c6164d86a6c248b79903a901c184f547'  # Reemplaza con tu clave API real
 
-gemini_pro, gemini_vision = st.tabs(["Gemini Pro", "Gemini Pro Vision"])
+deployment_name = "bcu-gpt-4"
+
+llm = AzureOpenAI(
+    deployment_name=deployment_name,
+    api_version="2023-05-15",
+    api_base=OPENAI_API_BASE,
+    api_token=OPENAI_API_KEY,
+    is_chat_model=True,
+)
+
+# Configuración de la barra lateral
+def setup_sidebar():
+    st.sidebar.image("bcu.png", width=180)
+    st.sidebar.title('Gemini AI Explorer')
+    st.sidebar.markdown('''
+    **Instrucciones de uso**
+    
+    - Utiliza Gemini Pro para interactuar con modelos de lenguaje avanzados mediante texto.
+    - Explora Gemini Pro Vision cargando imágenes y proporcionando un texto de entrada para obtener resultados visuales enriquecidos.
+    - Navega entre las pestañas de Gemini Pro y Gemini Pro Vision para experimentar con diferentes tipos de entradas y modelos.
+    ''')
+
+    st.sidebar.markdown('''
+    **Acerca de la aplicación**
+    
+    - Gemini AI Explorer te permite explorar las capacidades de los modelos de inteligencia artificial de Gemini Pro y Gemini Pro Vision.
+    - Desarrollada utilizando Streamlit, esta aplicación facilita la interacción con modelos de IA de última generación.
+    ''')
+    st.sidebar.write('Desarrollado por [Diego Fernández](https://www.linkedin.com/in/diego-fernandez-728a35206/)')
+
+    st.sidebar.markdown('''
+    **Grupo de Desarrollo**
+    
+    Esta aplicación ha sido desarrollada en el grupo **ARISE: Artificial Intelligence Research in Statistics and Economics** del Banco Central del Uruguay.
+    ''')
 
 def main():
-    with gemini_pro:
-        st.header("Interact with Gemini Pro")
-        st.write("")
+    setup_sidebar()
 
-        prompt = st.text_input("prompt please...", placeholder="Prompt", label_visibility="visible")
+    st.image("./Google-Gemini-AI-Logo.png", width=200)
+
+    gemini_pro, gemini_vision = st.tabs(["Gemini Pro", "Gemini Pro Vision"])
+
+    with gemini_pro:
+        st.header("Interactua con Gemini Pro")
+
+        prompt = st.text_input("Ingresa prompt por favor...", placeholder="Prompt")
         model = genai.GenerativeModel("gemini-pro")
 
-        if st.button("SEND",use_container_width=True):
+        if st.button("SEND", use_container_width=True):
             response = model.generate_content(prompt)
-
-            st.write("")
-            st.header(":blue[Response]")
-            st.write("")
-
             st.markdown(response.text)
 
     with gemini_vision:
-        st.header("Interact with Gemini Pro Vision")
-        st.write("")
+        st.header("Interactua con Gemini Pro Vision")
 
-        image_prompt = st.text_input("Interact with the Image", placeholder="Prompt", label_visibility="visible")
-        uploaded_file = st.file_uploader("Choose and Image", accept_multiple_files=False, type=["png", "jpg", "jpeg", "img", "webp"])
+        image_prompt = st.text_input("Interactua con la imagen", placeholder="Prompt")
+        uploaded_file = st.file_uploader("Escoge una imagen", accept_multiple_files=False, type=["png", "jpg", "jpeg", "img", "webp"])
 
         if uploaded_file is not None:
-            st.image(Image.open(uploaded_file), use_column_width=True)
+            image = Image.open(uploaded_file)
+            st.image(image, use_column_width=True)
 
-            st.markdown("""
-                <style>
-                        img {
-                            border-radius: 10px;
-                        }
-                </style>
-                """, unsafe_allow_html=True)
-            
         if st.button("GET RESPONSE", use_container_width=True):
             model = genai.GenerativeModel("gemini-pro-vision")
 
             if uploaded_file is not None:
                 if image_prompt != "":
-                    image = Image.open(uploaded_file)
-
                     response = model.generate_content(
                         glm.Content(
-                            parts = [
+                            parts=[
                                 glm.Part(text=image_prompt),
                                 glm.Part(
                                     inline_data=glm.Blob(
@@ -79,22 +110,12 @@ def main():
                             ]
                         )
                     )
-
                     response.resolve()
-
-                    st.write("")
-                    st.write(":blue[Response]")
-                    st.write("")
-
                     st.markdown(response.text)
-
                 else:
-                    st.write("")
-                    st.header(":red[Please Provide a prompt]")
-
+                    st.error("Por favor carga una imagen")
             else:
-                st.write("")
-                st.header(":red[Please Provide an image]")
+                st.error("Por favor carga una imagen")
 
 if __name__ == "__main__":
     main()
